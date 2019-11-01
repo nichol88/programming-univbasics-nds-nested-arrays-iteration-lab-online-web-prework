3fac458b52cbedddbd47004f88f4b566929446e3
Brian Nicholls <nichol88@gmail.com>
2019-11-01 07:57:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -19,7 +19,7 @@ def join_ingredients(src)
 
     row_index += 1
 
-    output.push(["I love #{new_array[0]} and #{new_array[1]} on my pizza"])
+    output.push("I love #{new_array[0]} and #{new_array[1]} on my pizza")
     output
   end
 
